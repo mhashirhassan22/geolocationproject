87bf81d5dc8d6c595071ddcb46ca1bf7ef872464
mhashirhassan22 <mhashirhassan22@gmail.com>
2021-03-08 11:04:34 +0500

    f

--- pictures/admin.py
+++ pictures/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Location, Favorites
 
 # Register your models here.
+
+class LocAdmin(admin.ModelAdmin):
+    list_display = ('name', 'latitude','longitude','user')
+
+class FavAdmin(admin.ModelAdmin):
+    list_display = ('server', 'photo_id', 'secret', 'user')
+
+admin.site.register(Location, LocAdmin)
+admin.site.register(Favorites, FavAdmin)
